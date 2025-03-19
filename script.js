81e2f2b7cfa643248fdedb998f297e7ee54f5154
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-03-20 01:22:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+
+let userAge = document.querySelector("#age");
+let userName = document.querySelector("#name");
+let submitBtn = document.querySelector("#btn");
+let message = document.querySelector("#message");
+
+submitBtn.addEventListener("click", (e)=>{
+	e.preventDefault();
+	if(userAge.value == "" || userName.value == ""){
+		message.style.display = "block";
+	};
+
+	if(userAge.value > 18){
+		setTimeout(()=>{
+			alert("Welcome, John. You can vote.");
+		}, 4000);
+	} else {
+		setTimeout(()=>{
+			alert("Oh sorry Doe. You aren't old enough.");
+		}, 4000);
+	}
+	
+});
